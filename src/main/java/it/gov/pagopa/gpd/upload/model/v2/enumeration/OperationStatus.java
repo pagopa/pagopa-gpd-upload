81c6da900b8436d234938e4cad7a9758334fcec0
svariant <samuele.varianti@nttdata.com>
2025-09-24 11:08:53 +0200

    chore: [PAGOPA-3280] Rename operationStatus enums

--- src/main/java/it/gov/pagopa/gpd/upload/model/v2/enumeration/OperationStatus.java
+++ src/main/java/it/gov/pagopa/gpd/upload/model/v2/enumeration/OperationStatus.java
@@ -1,5 +1,5 @@
 package it.gov.pagopa.gpd.upload.model.v2.enumeration;
 
 public enum OperationStatus {
-    IN_PROGRESS, COMPLETED, COMPLETED_PARTIALLY, FAILED
+    IN_PROGRESS, COMPLETED, COMPLETED_WITH_WARNINGS, COMPLETED_UNSUCCESSFULLY
 }
