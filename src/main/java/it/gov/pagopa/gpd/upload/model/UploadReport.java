15823abce34596561b3e972d876f05bfe13093c2
Francesco <cesareo.francesco@gmail.com>
2025-09-26 09:13:55 +0200

    fix unit test

--- src/main/java/it/gov/pagopa/gpd/upload/model/UploadReport.java
+++ src/main/java/it/gov/pagopa/gpd/upload/model/UploadReport.java
@@ -25,11 +25,9 @@ public class UploadReport {
 
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
     @Schema(example = "2024-10-08T14:55:16.302Z")
-    @JsonSerialize(as = LocalDateTimeSerializer.class)
     public LocalDateTime startTime;
 
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
     @Schema(example = "2024-10-08T14:55:16.302Z")
-    @JsonSerialize(as = LocalDateTimeSerializer.class)
     public LocalDateTime endTime;
 }
