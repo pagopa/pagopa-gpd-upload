0d11b96d23a02f37e1d03a48680a93117d637a34
svariant <samuele.varianti@nttdata.com>
2025-10-01 12:51:55 +0200

    chore: BlobService improve unit test

--- src/test/java/it/gov/pagopa/gpd/upload/service/BlobServiceTest.java
+++ src/test/java/it/gov/pagopa/gpd/upload/service/BlobServiceTest.java
@@ -1,35 +1,53 @@
 package it.gov.pagopa.gpd.upload.service;
 
+import com.azure.core.util.BinaryData;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.micronaut.http.HttpStatus;
 import io.micronaut.http.multipart.CompletedFileUpload;
+import it.gov.pagopa.gpd.upload.entity.ResponseEntry;
 import it.gov.pagopa.gpd.upload.exception.AppException;
+import it.gov.pagopa.gpd.upload.model.UploadInput;
 import it.gov.pagopa.gpd.upload.model.UploadOperation;
 import it.gov.pagopa.gpd.upload.model.enumeration.ServiceType;
 import it.gov.pagopa.gpd.upload.model.pd.MultipleIUPDModel;
 import it.gov.pagopa.gpd.upload.model.pd.PaymentPositionsModel;
+import it.gov.pagopa.gpd.upload.model.v1.UploadReport;
+import it.gov.pagopa.gpd.upload.model.v2.UploadReportDTO;
 import it.gov.pagopa.gpd.upload.repository.BlobStorageRepository;
 import it.gov.pagopa.gpd.upload.utils.FileUtils;
 import it.gov.pagopa.gpd.upload.utils.GPDValidator;
 import it.gov.pagopa.gpd.upload.utils.ResponseEntryDTOMapperImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+import static it.gov.pagopa.gpd.upload.utils.Constants.INPUT_DIRECTORY;
+import static it.gov.pagopa.gpd.upload.utils.Constants.OUTPUT_DIRECTORY;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class BlobServiceTest {
     private static final String FISCAL_CODE = "fiscal-code";
     private static final String BROKER_CODE = "broker-code";
-
-    private static BlobStorageRepository blobStorageRepository = mock(BlobStorageRepository.class);
-    private static StatusService statusService = mock(StatusService.class);
-    private static BlobService blobService = new BlobService(blobStorageRepository, statusService, new GPDValidator<PaymentPositionsModel>(), new GPDValidator<MultipleIUPDModel>(), new ResponseEntryDTOMapperImpl());
+    private static final String FILE_ID = "fileId";
+    private static final String IUPD_1 = "IUPD1";
+    private static final String IUPD_2 = "IUPD2";
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final BinaryData binaryData = mock(BinaryData.class);
+    private static final BlobStorageRepository blobStorageRepository = mock(BlobStorageRepository.class);
+    private static final StatusService statusService = mock(StatusService.class);
+    private static final BlobService blobService = new BlobService(blobStorageRepository, statusService, new GPDValidator<PaymentPositionsModel>(), new GPDValidator<MultipleIUPDModel>(), new ResponseEntryDTOMapperImpl());
 
     @BeforeAll
     static void beforeAll() throws NoSuchFieldException, IllegalAccessException {
@@ -42,6 +60,11 @@ class BlobServiceTest {
         zipMaxEntriesField.setInt(blobService, 2);
 
         blobService.init();
+    }
+
+    @BeforeEach
+    void beforeEach(){
+        reset(blobStorageRepository, statusService);
     }
 
     @Test
@@ -91,5 +114,50 @@ class BlobServiceTest {
         CompletedFileUpload file = FileUtils.getUpsertFile();
 
         assertThrows(AppException.class, () -> blobService.delete(BROKER_CODE, FISCAL_CODE, UploadOperation.CREATE, file, ServiceType.GPD));
+    }
+
+    @Test
+    void getReportV1_OK() throws JsonProcessingException {
+        UploadReport uploadReport = UploadReport.builder().uploadID(FILE_ID).build();
+        when(binaryData.toString()).thenReturn(objectMapper.writeValueAsString(uploadReport));
+
+        when(blobStorageRepository.downloadContent(BROKER_CODE, FILE_ID, String.format("/%s/%s/report%s.json", FISCAL_CODE, OUTPUT_DIRECTORY, FILE_ID), ServiceType.GPD)).thenReturn(binaryData);
+
+        UploadReport response = blobService.getReportV1(BROKER_CODE, FISCAL_CODE, FILE_ID, ServiceType.GPD);
+        assertEquals(uploadReport.getUploadID(), response.getUploadID());
+        verify(blobStorageRepository, times(1)).downloadContent(BROKER_CODE, FILE_ID, String.format("/%s/%s/report%s.json", FISCAL_CODE, OUTPUT_DIRECTORY, FILE_ID), ServiceType.GPD);
+    }
+
+    @Test
+    void getReportV2_OK() throws JsonProcessingException {
+        UploadReport uploadReport = UploadReport.builder()
+                .uploadID(FILE_ID)
+                .responses(new ArrayList<>(List.of(
+                        ResponseEntry.builder().requestIDs(Collections.singletonList(IUPD_1)).statusCode(HttpStatus.OK.getCode()).build(),
+                        ResponseEntry.builder().requestIDs(Collections.singletonList(IUPD_2)).statusCode(HttpStatus.INTERNAL_SERVER_ERROR.getCode()).build()
+                )))
+                .build();
+        when(binaryData.toString()).thenReturn(objectMapper.writeValueAsString(uploadReport));
+
+        when(blobStorageRepository.downloadContent(BROKER_CODE, FILE_ID, String.format("/%s/%s/report%s.json", FISCAL_CODE, OUTPUT_DIRECTORY, FILE_ID), ServiceType.GPD)).thenReturn(binaryData);
+
+        UploadReportDTO response = blobService.getReportV2(BROKER_CODE, FISCAL_CODE, FILE_ID, ServiceType.GPD);
+        assertEquals(uploadReport.getUploadID(), response.getFileId());
+        Assertions.assertTrue(response.getResponses().get(0).getIupds().contains(IUPD_1));
+        Assertions.assertTrue(response.getResponses().get(1).getIupds().contains(IUPD_2));
+        verify(blobStorageRepository, times(1)).downloadContent(BROKER_CODE, FILE_ID, String.format("/%s/%s/report%s.json", FISCAL_CODE, OUTPUT_DIRECTORY, FILE_ID), ServiceType.GPD);
+    }
+
+    @Test
+    void getUploadInput_OK() throws JsonProcessingException {
+        UploadInput uploadInput = UploadInput.builder().paymentPositionIUPDs(List.of(IUPD_1, IUPD_2)).build();
+        when(binaryData.toString()).thenReturn(objectMapper.writeValueAsString(uploadInput));
+
+        when(blobStorageRepository.downloadContent(BROKER_CODE, FILE_ID, String.format("/%s/%s/%s.json", FISCAL_CODE, INPUT_DIRECTORY, FILE_ID), ServiceType.GPD)).thenReturn(binaryData);
+
+        UploadInput response = blobService.getUploadInput(BROKER_CODE, FISCAL_CODE, FILE_ID, ServiceType.GPD);
+        assertEquals(uploadInput.getPaymentPositionIUPDs().get(0), response.getPaymentPositionIUPDs().get(0));
+        assertEquals(uploadInput.getPaymentPositionIUPDs().get(1), response.getPaymentPositionIUPDs().get(1));
+        verify(blobStorageRepository, times(1)).downloadContent(BROKER_CODE, FILE_ID, String.format("/%s/%s/%s.json", FISCAL_CODE, INPUT_DIRECTORY, FILE_ID), ServiceType.GPD);
     }
 }
